6efad33acfec9b1d7118710d484dc987bb4c07c5
webdevpathologist <71933275+webdevpathologist@users.noreply.github.com>
2022-01-03 15:20:37 +0530

    first commit

---
+++ precache-manifest.c63e6c2ddb799222242c3ad53e583d47.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62bc3e4552cdd8e9d43186233df46a09",
+    "url": "/index.html"
+  },
+  {
+    "revision": "121b1af5866e06682d7a",
+    "url": "/static/css/main.e824f6eb.chunk.css"
+  },
+  {
+    "revision": "48b737dd899e56fa1bef",
+    "url": "/static/js/2.9696daf6.chunk.js"
+  },
+  {
+    "revision": "121b1af5866e06682d7a",
+    "url": "/static/js/main.103da5e7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);
